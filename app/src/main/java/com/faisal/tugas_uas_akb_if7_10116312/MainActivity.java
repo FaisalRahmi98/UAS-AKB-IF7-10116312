009e310ad5c80483c07609684202832123afa71f
Faisal <rahmi.faisal@gmail.com>
2019-08-14 15:31:54 +0700

    Awal

---
+++ app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MainActivity.java
@@ -0,0 +1,13 @@
+package com.faisal.tugas_uas_akb_if7_10116312;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
