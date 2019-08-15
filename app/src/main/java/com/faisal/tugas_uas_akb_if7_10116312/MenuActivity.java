15afb7260575d8ee2cd1960e3a8ee3ebcdb467cd
Faisal <rahmi.faisal@gmail.com>
2019-08-16 00:35:49 +0700

    Menu

--- app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MenuActivity.java
+++ app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MenuActivity.java
@@ -1,7 +1,12 @@
 package com.faisal.tugas_uas_akb_if7_10116312;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+
+        final ProfilFragment profilFragment = new ProfilFragment();
+        final KontakFragment kontakFragment = new KontakFragment();
+        final TemanFragment temanFragment = new TemanFragment();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if(id == R.id.idprofil){
+                    setFragment(profilFragment);
+                    return true;
+                } else if(id == R.id.idkontak){
+                    setFragment(kontakFragment);
+                    return true;
+                } else if(id == R.id.idteman){
+                    setFragment(temanFragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        navigationView.setSelectedItemId(R.id.idprofil);
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
     }
 }
