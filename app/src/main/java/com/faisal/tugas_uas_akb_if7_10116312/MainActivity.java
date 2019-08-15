9201cdf637b423348a6f177ad73a8d908a904702
Faisal <rahmi.faisal@gmail.com>
2019-08-15 19:59:21 +0700

    ViewPager

--- app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MainActivity.java
+++ app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MainActivity.java
@@ -1,13 +1,36 @@
 package com.faisal.tugas_uas_akb_if7_10116312;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.faisal.tugas_uas_akb_if7_10116312.fragments.PageFragment1;
+import com.faisal.tugas_uas_akb_if7_10116312.fragments.PageFragment2;
+import com.faisal.tugas_uas_akb_if7_10116312.fragments.PageFragment3;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new PageFragment1());
+        list.add(new PageFragment2());
+        list.add(new PageFragment3());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(),list);
+
+        pager.setAdapter(pagerAdapter);
     }
 }
