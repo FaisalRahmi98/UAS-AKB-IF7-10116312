ab55d65b533a1a99843f43229cb5e5746379ed54
Faisal <rahmi.faisal@gmail.com>
2019-08-15 18:13:05 +0700

    Revisi Splash Screen

--- app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/SplashActivity.java
+++ app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/SplashActivity.java
@@ -1,13 +1,28 @@
 package com.faisal.tugas_uas_akb_if7_10116312;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try{
+                    sleep(4000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
 }
