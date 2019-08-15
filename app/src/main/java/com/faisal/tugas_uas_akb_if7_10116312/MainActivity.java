54d1968e0f0ac0831d339900aac7159e1afcbbf9
Faisal <rahmi.faisal@gmail.com>
2019-08-15 21:11:27 +0700

    Revisi ViewPager

--- app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MainActivity.java
+++ app/src/main/java/com/faisal/tugas_uas_akb_if7_10116312/MainActivity.java
@@ -1,10 +1,12 @@
 package com.faisal.tugas_uas_akb_if7_10116312;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.faisal.tugas_uas_akb_if7_10116312.fragments.PageFragment1;
 import com.faisal.tugas_uas_akb_if7_10116312.fragments.PageFragment2;
@@ -32,5 +34,11 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(),list);
 
         pager.setAdapter(pagerAdapter);
+    }
+
+    public void openMasuk(View view) {
+        Intent intent = new Intent(MainActivity.this, MasukActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
